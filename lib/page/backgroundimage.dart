8141ee60da58bd9f66e8eb958629fd1f0d94b6a5
Said Office Pc <info@saidkurt.com>
2021-07-14 12:29:34 +0300

    ssss

--- lib/page/backgroundimage.dart
+++ lib/page/backgroundimage.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/const/screen.dart';
 
 class BackgroundImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: ScreenView(context).heightS,
-      width: ScreenView(context).widthS,
-      decoration: new BoxDecoration(
-        color: Colors.purple,
-        gradient: new LinearGradient(
-            colors: [Colors.white, Colors.green.withOpacity(0.6), Colors.white],
-            begin: Alignment.bottomLeft,
-            end: Alignment.topLeft),
+    return ShaderMask(
+      shaderCallback: (bounds) => LinearGradient(colors: [
+        Colors.white,
+        Colors.red.withOpacity(0.8),
+        Colors.red.withOpacity(0.8),
+        Colors.white,
+      ], begin: Alignment.topRight, end: Alignment.bottomLeft)
+          .createShader(bounds),
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("assets/3.jpg"),
+          fit: BoxFit.cover,
+        )),
       ),
     );
   }
